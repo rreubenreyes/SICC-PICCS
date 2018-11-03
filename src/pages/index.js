2546404937349c9c3187cd338f086edfd90a20e7
Reuben Reyes <reuben.a.reyes@gmail.com>
2018-11-03 14:45:56 -0700

    feat(Index): add loading screen

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,29 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'gatsby'
 
 import Layout from '../components/Layout'
 import Image from '../components/Image'
 import Logo from '../images/logo.svg'
 
-const IndexPage = () => (
-  <Layout>
-    <Logo />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+export default class IndexPage extends Component {
+  state = {
+    loadingStatus: '',
+  }
 
-export default IndexPage
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({ loadingStatus: 'hidden' })
+    }, 1000)
+  }
+
+  render() {
+    const { loadingStatus } = this.state
+    return (
+      <Layout>
+        <div className={`loading-screen ${loadingStatus}`}>
+          <Logo />
+        </div>
+      </Layout>
+    )
+  }
+}
