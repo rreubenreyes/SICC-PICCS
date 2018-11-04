51b3b9241bab043fc80903aca788e78402ebbde0
Reuben Reyes <reuben.a.reyes@gmail.com>
2018-11-04 14:33:41 -0800

    fix(GameInProgress): get game id props from Challenge

--- src/components/Challenge.js
+++ src/components/Challenge.js
@@ -1,34 +1,34 @@
-import React, { Component } from "react";
-import gql from "graphql-tag";
-import { Query } from "react-apollo";
+import React, { Component } from 'react'
+import gql from 'graphql-tag'
+import { Query } from 'react-apollo'
 
 class Challenge extends Component {
   render() {
-    const { gameDataId } = this.props;
+    const { gameDataId } = this.props
     const GAME_DATA_QUERY = gql`
       query {
-        game_data(where: {id: ${gameDataId}}) {
+        game_data(where: {id: { _eq: ${gameDataId}}}) {
           keyword
         }
       }
-    `;
+    `
 
     return (
       <Query query={GAME_DATA_QUERY}>
         {({ loading, error, data = {} }) => {
-          const { game_data = [] } = data;
+          const { game_data = [] } = data
           if (game_data.length === 1) {
             return (
               <p>{`Your challenge is to take a picture of a ${
                 game_data[0].keyword
               }`}</p>
-            );
+            )
           }
-          return null;
+          return null
         }}
       </Query>
-    );
+    )
   }
 }
 
-export default Challenge;
+export default Challenge
