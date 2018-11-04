5948f01f02e1e3ce527071481a0bbc62dceecf78
Reuben Reyes <reuben.a.reyes@gmail.com>
2018-11-04 11:59:25 -0800

    refactor: move Game subscription to PlayGame

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
-import Start from "../components/Start";
-import gql from "graphql-tag";
-import { Subscription } from "react-apollo";
-import PlayGame from "../components/PlayGame";
+import React, { Component } from 'react'
+import Start from '../components/Start'
+import gql from 'graphql-tag'
+import { Subscription } from 'react-apollo'
+import PlayGame from '../components/PlayGame'
 
 class Game extends Component {
   render() {
@@ -10,34 +10,14 @@ class Game extends Component {
       location: {
         state: { createdByUser, userId, gameId = null }
       }
-    } = this.props;
-    console.log({ userId, gameId });
+    } = this.props
+    console.log({ userId, gameId })
     if (createdByUser) {
-      return <Start userId={userId} gameId={gameId} />;
+      return <Start userId={userId} gameId={gameId} />
     }
 
-    const GAMES_SUBSCRIPTION = gql`
-      subscription GamesSubscription {
-        games(where: { status: { _eq: "inProgress" }, id: {_eq: "${gameId}"} }) {
-          id
-          createdBy
-          status
-        }
-      }
-    `;
-    return (
-      <Subscription subscription={GAMES_SUBSCRIPTION}>
-        {({ data = {}, error, loading }) => {
-          console.log({ data, error, loading });
-          const { games = [] } = data;
-          if (games.length === 1) {
-            return <PlayGame userId={userId} gameId={gameId} />;
-          }
-          return <h3>You are waiting for the game to start</h3>;
-        }}
-      </Subscription>
-    );
+    return <PlayGame userId={userId} gameId={gameId} />
   }
 }
 
-export default Game;
+export default Game
