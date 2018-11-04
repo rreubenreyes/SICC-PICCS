1dfd54838f08e9ad77ba78a22fe495d27657d1d4
Reuben Reyes <reuben.a.reyes@gmail.com>
2018-11-03 19:12:43 -0700

    link apollo boost to eract

--- src/App.js
+++ src/App.js
@@ -2,25 +2,12 @@ import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-import { ApolloClient } from 'apollo-boost'
-import { ApolloProvider } from 'react-apollo'
-import { HttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-
-const client = new ApolloClient({
-  uri: 'http://sicc-piccs-api.herokuapp.com/',
-  link: new HttpLink(),
-  cache: new InMemoryCache()
-})
-
 class App extends Component {
   render() {
     return (
-      <ApolloProvider client={client}>
-        <div>
-          <h2>hello apollo</h2>
-        </div>
-      </ApolloProvider>
+      <div>
+        <h2>hello apollo</h2>
+      </div>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,50 @@ import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import { ApolloClient } from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+import { HttpLink } from 'apollo-link-http'
+import { WebSocketLink } from 'apollo-link-ws'
+import { SubscriptionClient } from 'subscriptions-transport-ws'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { split } from 'apollo-link'
+import { getMainDefinition } from 'apollo-utilities'
+
+const GQL_ENDPOINT = 'http://sicc-piccs-api.herokuapp.com/v1alpha1/graphql'
+
+const createWebsocketLink = uri => {
+  const splitUri = uri.split('//')
+  const subscriptionClient = new SubscriptionClient(`wss://${splitUri[1]}`, {
+    reconnect: true
+  })
+
+  return new WebSocketLink(subscriptionClient)
+}
+
+const httpLink = new HttpLink({ uri: GQL_ENDPOINT })
+const wsLink = createWebsocketLink(GQL_ENDPOINT)
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  httpLink
+)
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache({
+    addTypename: false
+  })
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
