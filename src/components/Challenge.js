bafde86ad1e57b812257d9efd27ce2b6ef36b890
austintackaberry <austintackaberry@gmail.com>
2018-11-04 12:33:36 -0800

    Make Challenge component

---
+++ src/components/Challenge.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+
+class Challenge extends Component {
+  render() {
+    const { gameDataId } = this.props;
+    const GAME_DATA_QUERY = gql`
+      query {
+        game_data(where: {id: ${gameDataId}}) {
+          keyword
+        }
+      }
+    `;
+
+    return (
+      <Query query={GAME_DATA_QUERY}>
+        {({ loading, error, data = {} }) => {
+          const { game_data = [] } = data;
+          if (game_data.length === 1) {
+            return (
+              <p>{`Your challenge is to take a picture of a ${
+                game_data[0].keyword
+              }`}</p>
+            );
+          }
+          return null;
+        }}
+      </Query>
+    );
+  }
+}
+
+export default Challenge;
