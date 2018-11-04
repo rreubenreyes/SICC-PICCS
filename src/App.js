a5639964598ebec509ab304a9f1377f9166375f6
austintackaberry <austintackaberry@gmail.com>
2018-11-03 20:39:50 -0700

    Create new game and user onClick of create

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import Create from "./components/Create";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div>
         <h2>hello apollo</h2>
+        <Create />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
