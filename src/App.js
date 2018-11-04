b372f2713dbc1cb0215586d54380549904bef2a8
Reuben Reyes <reuben.a.reyes@gmail.com>
2018-11-03 19:57:35 -0700

    add react-router

--- src/App.js
+++ src/App.js
@@ -1,63 +1,22 @@
-import React, { Component } from 'react';
-const Clarifai = require('clarifai');
+import React, { Component } from 'react'
 
-const clarifaiApp = new Clarifai.App({
-    apiKey: '78812b999a7e4e76b5c5765356651516',
-});
+import { withRouter } from 'react-router'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import ClarifaiTest from './components/ClarifaiTest'
+import Home from './components/Home'
 
 class App extends Component {
-    state = {
-        imageURL: '',
-        results: false,
-    };
-
-    handleUpload = async e => {
-        const files = e.target.files;
-        const data = new FormData();
-        data.append('file', files[0]);
-        data.append('upload_preset', 'siccpiccs');
-        const res = await fetch('https://api.cloudinary.com/v1_1/brandonstinson/image/upload', {
-            method: 'POST',
-            body: data,
-        });
-        const file = await res.json();
-        this.setState({ imageURL: file.secure_url }, this.getClarifaiData);
-    };
-
-    getClarifaiData = () => {
-        const { imageURL } = this.state;
-        clarifaiApp.models
-            .predict('bd367be194cf45149e75f01d59f77ba7', imageURL)
-            .then(res => this.setState({ results: res }));
-    };
-
-    render() {
-        const { results } = this.state;
-        return (
-            <React.Fragment>
-                <h1>Take a picture!</h1>
-                <input
-                    type="file"
-                    accept="image/*"
-                    capture
-                    id="provided-image"
-                    onChange={this.handleUpload}
-                    placeholder="Click to open your camera"
-                />
-                <h1>Results:</h1>
-                <div style={{ margin: '20px' }}>
-                    {results &&
-                        results.outputs[0].data.concepts.map(item => (
-                            <p key={item.id}>
-                                {item.name}
-                                --
-                                {item.value}
-                            </p>
-                        ))}
-                </div>
-            </React.Fragment>
-        );
-    }
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/test" component={ClarifaiTest} />
+          <Route component={404} />
+        </Switch>
+      </Router>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/components/404.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const NotFound = () => {
+  return <div>Oops! This page doesn't ex</div>
+}
+
+export default NotFound
