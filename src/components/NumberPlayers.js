505ee01b164e0bf5440551b57a76e3ea980fcfd4
austintackaberry <austintackaberry@gmail.com>
2018-11-04 13:46:35 -0800

    Add number players component

---
+++ src/components/NumberPlayers.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+
+class NumberPlayers extends Component {
+  render() {
+    const { gameId } = this.props;
+    const NUMBER_PLAYERS_QUERY = gql`
+        query {
+          users(where: { gameId: { _eq: "${gameId}" } }) {
+            id
+          }
+        }
+      `;
+    return (
+      <Query query={NUMBER_PLAYERS_QUERY}>
+        {({ loading, error, data = {} }) => {
+          const { users = [] } = data;
+          if (!loading) {
+            let players = "players";
+            if (users.length === 1) {
+              players = "player";
+            }
+            return (
+              <h3>{`There are ${users.length} ${players} in the game`}</h3>
+            );
+          }
+          return null;
+        }}
+      </Query>
+    );
+  }
+}
+
+export default NumberPlayers;
