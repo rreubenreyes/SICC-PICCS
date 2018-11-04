c5db52ae7d91132bc64c2519d783d4b0df6b7dde
Reuben Reyes <reuben.a.reyes@gmail.com>
2018-11-03 18:29:08 -0700

    link apollo boost to react

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+import { ApolloClient } from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+const client = new ApolloClient({
+  uri: 'http://sicc-piccs-api.herokuapp.com/',
+  link: new HttpLink(),
+  cache: new InMemoryCache()
+})
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <ApolloProvider client={client}>
+        <div>
+          <h2>hello apollo</h2>
+        </div>
+      </ApolloProvider>
+    )
   }
 }
 
-export default App;
+export default App
