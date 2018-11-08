101a40c0f9426450a1a620c901875ea4ac0f621c
austintackaberry <austintackaberry@gmail.com>
2018-11-07 22:46:50 -0800

    Remove Clarifai test

--- src/App.js
+++ src/App.js
@@ -1,41 +1,40 @@
-import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import ClarifaiTest from './components/ClarifaiTest'
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Header from './components/Header'
-import Home from './pages/Home'
-import NoMatch from './components/404'
-import Game from './pages/Game'
-import Close from './pages/Close'
-import logo from './static/svgs/logo.svg'
-import './styles/style.scss'
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import NoMatch from "./pages/404";
+import Game from "./pages/Game";
+import Close from "./pages/Close";
+import logo from "./static/svgs/logo.svg";
+import "./styles/style.scss";
 
-const LOADING_PHRASE = 'Welcome to'
+const LOADING_PHRASE = "Welcome to";
 
 function* typewriter() {
   for (let letter of LOADING_PHRASE) {
-    yield letter
+    yield letter;
   }
 }
 
 class App extends Component {
   state = {
     typewriterGenerator: typewriter(),
-    logoClassName: '',
-    loadingStateClassName: '',
+    logoClassName: "",
+    loadingStateClassName: "",
     loadingPhrase: {
-      value: '',
+      value: "",
       done: false
     }
-  }
+  };
 
   componentDidMount = () => {
     setTimeout(() => {
       this.setState({
-        loadingStateClassName: 'loaded'
-      })
-    }, 3000)
-  }
+        loadingStateClassName: "loaded"
+      });
+    }, 3000);
+  };
 
   render() {
     const {
@@ -43,25 +42,25 @@ class App extends Component {
       typewriterGenerator,
       loadingPhrase,
       logoClassName
-    } = this.state
-    const nextLetter = typewriterGenerator.next().value
+    } = this.state;
+    const nextLetter = typewriterGenerator.next().value;
     if (nextLetter) {
       setTimeout(() => {
         this.setState({
           loadingPhrase: {
             value: loadingPhrase.value + nextLetter
           }
-        })
-      }, 150)
+        });
+      }, 150);
     } else {
       setTimeout(() => {
         this.setState({
           loadingPhrase: {
-            value: ''
+            value: ""
           },
-          logoClassName: 'show'
-        })
-      }, 650)
+          logoClassName: "show"
+        });
+      }, 650);
     }
     return (
       <>
@@ -79,15 +78,14 @@ class App extends Component {
             <Switch>
               <Route exact path="/" component={Home} />
               <Route exact path="/lobby" component={Game} />
-              <Route exact path="/test" component={ClarifaiTest} />
               <Route exact path="/closeAllGames" component={Close} />
               <Route component={NoMatch} />
             </Switch>
           </Router>
         </div>
       </>
-    )
+    );
   }
 }
 
-export default App
+export default App;
