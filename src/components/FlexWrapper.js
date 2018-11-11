d3a408b4273efeb2544b2f84183edf24898eb604
Reuben Reyes <reuben.a.reyes@gmail.com>
2018-11-11 12:32:55 -0800

    chore: add linting tools

--- src/components/FlexWrapper.js
+++ src/components/FlexWrapper.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const FlexWrapper = ({ direction = 'row', wrapping = 'nowrap', children }) => {
-  return <div className={`flex`}>{children()}</div>
-}
+  return <div className={`flex`}>{children()}</div>;
+};
 
-export default FlexWrapper
+export default FlexWrapper;
