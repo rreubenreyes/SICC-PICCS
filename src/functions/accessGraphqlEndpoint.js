9e57ec4a8ad0ec9fca8f0f3f716d3c8454e556bc
Reuben Reyes <reuben.a.reyes@gmail.com>
2018-11-12 10:45:11 -0800

    add auth link to Apollo

---
+++ src/functions/accessGraphqlEndpoint.js
@@ -0,0 +1,4 @@
+exports.handler = function(event, context, callback) {
+  console.log('EVENT:', event, '\nCONTEXT:', context);
+  console.log(process);
+};
--- src/index.js
+++ src/index.js
@@ -1,54 +1,68 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import * as serviceWorker from './serviceWorker'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-import { ApolloClient } from 'apollo-boost'
-import { ApolloProvider } from 'react-apollo'
-import { HttpLink } from 'apollo-link-http'
-import { WebSocketLink } from 'apollo-link-ws'
-import { SubscriptionClient } from 'subscriptions-transport-ws'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import { split } from 'apollo-link'
-import { getMainDefinition } from 'apollo-utilities'
+import { ApolloClient } from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import { getMainDefinition } from 'apollo-utilities';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { setContext } from 'apollo-link-context';
+import { split } from 'apollo-link';
+import { SubscriptionClient } from 'subscriptions-transport-ws';
+import { WebSocketLink } from 'apollo-link-ws';
 
-const GQL_ENDPOINT = 'https://sicc-piccs-api.herokuapp.com/v1alpha1/graphql'
+const GQL_ENDPOINT = 'https://sicc-piccs-api.herokuapp.com/v1alpha1/graphql';
 
 const createWebsocketLink = uri => {
-  const splitUri = uri.split('//')
+  const splitUri = uri.split('//');
   const subscriptionClient = new SubscriptionClient(`wss://${splitUri[1]}`, {
-    reconnect: true
-  })
+    reconnect: true,
+  });
 
-  return new WebSocketLink(subscriptionClient)
-}
+  return new WebSocketLink(subscriptionClient);
+};
 
-const httpLink = new HttpLink({ uri: GQL_ENDPOINT })
-const wsLink = createWebsocketLink(GQL_ENDPOINT)
+// set hasura access key
+const authLink = setContext((_, { headers }) => {
+  const accessKey = 'cKkwPfe09WEqy26CHGm2'; // TEMPORARY
+  return {
+    headers: {
+      ...headers,
+      'X-Hasura-Access-Key': accessKey || '',
+    },
+  };
+});
+
+// hasura endpoint URI
+const httpLink = authLink.concat(new HttpLink({ uri: GQL_ENDPOINT }));
+
+const wsLink = createWebsocketLink(GQL_ENDPOINT);
 const link = split(
   ({ query }) => {
-    const { kind, operation } = getMainDefinition(query)
-    return kind === 'OperationDefinition' && operation === 'subscription'
+    const { kind, operation } = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'subscription';
   },
   wsLink,
   httpLink
-)
+);
 
 const client = new ApolloClient({
   link,
   cache: new InMemoryCache({
-    addTypename: false
-  })
-})
+    addTypename: false,
+  }),
+});
 
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>,
   document.getElementById('root')
-)
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.unregister();
