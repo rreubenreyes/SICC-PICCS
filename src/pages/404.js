7712229008b0fd02af1d8a6ae0323702eb19e8d2
Vincent Tam <vtam2012@gmail.com>
2018-12-14 20:51:22 -0800

    fixed typo on 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const NoMatch = () => {
-  return <div>Oops! This page doesn't ex</div>
-}
+  return <div>Oops! This page doesn't exist</div>;
+};
 
-export default NoMatch
+export default NoMatch;
